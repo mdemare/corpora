bb01f1ba85fdb2752169c5acdf07b1292b542cf0
Michiel <mdemare@me.com>
2011-02-23 14:30:22 +0100

    new fanfiction importer

--- lib/fanfiction.rb
+++ lib/fanfiction.rb
@@ -1,8 +1,13 @@
 require 'net/http'
 require 'fileutils'
 
-story = ARGV[0].to_i
-INC = 1
+stories = if ARGV[0] =~ /\d+\.\.\d+/
+  s,e = ARGV[0].split("..")
+  (s.to_i .. e.to_i).to_a
+else
+  File.read(ARGV[0]).split("\n")
+end
+
 chapter = 1
 
 def save(html, story, chapter)
@@ -31,7 +36,7 @@ def save(html, story, chapter)
 end
 
 http = Net::HTTP.start "www.fanfiction.net"
-loop do
+stories.each do |story|
   u = "/s/#{story}/#{chapter}"
   begin
     h = http.get(u).body
@@ -47,35 +52,24 @@ loop do
   end
   if h.empty?
     puts "story #{story} is blank, skipping"
-    story += INC
   elsif h =~ /Story Not Found/
     # illegal id
     puts "story #{story} is illegal, skipping"
-    story += INC
   elsif h =~ /chapter navigation/
     # has chapters, save chapter, go to next
-    save(h,story,chapter) and story += INC and next
+    save(h,story,chapter) and next
     chapter += 1
   elsif chapter > 1
     # last chapter
     puts "last chapter found"
-    story += INC
     chapter = 1
   elsif h =~ /Message Type 1/
     # illegal id
     puts "story #{story} is illegal, skipping"
-    story += INC
     chapter = 1
   else
     # single chapter, save chapter, next story
     puts "single chapter story"
     save(h,story,chapter)
-    story += INC
-  end
-  if story % 100_000 == 0 and chapter == 1
-    s = "Reached story #{story}"
-    puts s
-    `say '#{s}'`
-    exit
   end
 end
