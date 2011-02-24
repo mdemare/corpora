4cda51914084afbe74ee5da5189169af5531259b
Michiel de Mare <mdemare@me.com>
2011-02-24 05:05:58 +0100

    improved fanfiction downloader

--- lib/fanfiction.rb
+++ lib/fanfiction.rb
@@ -1,5 +1,18 @@
+require 'tempfile'
 require 'net/http'
 require 'fileutils'
+
+if ARGV.size != 2
+  puts "usage: ruby fanfiction.rb story-id-file language"
+  exit
+end
+
+GITREPOS="/home/mdemare/corpora/raw-data"
+
+$tf = Tempfile.new("ff")
+$tf.chmod(0644)
+
+$outputfile = File.open("/home/mdemare/corpora/fanfiction-chapters-#{ARGV[1]}.txt",'w')
 
 stories = if ARGV[0] =~ /\d+\.\.\d+/
   s,e = ARGV[0].split("..")
@@ -18,58 +31,77 @@ def save(html, story, chapter)
     puts html.inspect
     exit
   end
-  if %w(dutch english spanish french italian german).include?(lang)
-    dir = if lang == 'english'
-      FileUtils.mkdir_p("/Users/mdemare/proj/datacorp/#{lang}/fanfiction.net/#{1+story/100000}00k")
-    else
-      FileUtils.mkdir_p("/Users/mdemare/proj/datacorp/#{lang}/fanfiction.net")
+  html =~ /storytext>/
+  html = $'
+  html =~ /Review this/
+  $tf.open
+  $tf.write($`)
+  $tf.close
+  
+  git_cmd = "git --git-dir #{GITREPOS} hash-object -w #{$tf.path}"
+  IO.popen(git_cmd) do |output|
+    line = [story,chapter,output.gets.chomp].join(?;)
+    $outputfile.puts line
+  end
+  
+end
+
+start = Time.now
+jstart = 0
+http = Net::HTTP.start "www.fanfiction.net"
+stories.each_with_index do |story,j|
+  if Time.now-start > 60
+    start = Time.now
+    speed = j-jstart
+    remaining = stories.size - j
+    STDERR.puts "speed: #{speed} per minute, remaining time: #{remaining/speed} minutes"
+    jstart = j
+  end
+  
+  loop do
+    u = "/s/#{story}/#{chapter}"
+    begin
+      h = http.get(u).body
+    rescue Exception
+      begin
+        http.close
+      rescue Exception
+      end
+      puts "network error, resuming in 5 minutes"
+      sleep 300
+      http = Net::HTTP.start "www.fanfiction.net"
+      retry
     end
-    html =~ /storytext>/
-    html = $'
-    html =~ /Review this/
-    File.open(File.join(dir,"#{story}-#{chapter}.txt"), 'w') {|f| f.write($`) }
-    puts "saving story(#{story}, chapter(#{chapter}) in #{lang})"
-  else
-    puts "skipping story in #{lang}"
-    :skip
+    if h.empty?
+      puts "story #{story} is blank, skipping"
+      break
+    elsif h =~ /Story Not Found/
+      # illegal id
+      puts "story #{story} is illegal, skipping"
+      break
+    elsif h =~ /chapter navigation/
+      # has chapters, save chapter, go to next
+      save(h,story,chapter) and next
+      chapter += 1
+    elsif chapter > 1
+      # last chapter
+      puts "last chapter found"
+      chapter = 1
+      break
+    elsif h =~ /Message Type 1/
+      # illegal id
+      puts "story #{story} is illegal, skipping"
+      chapter = 1
+      break
+    else
+      # single chapter, save chapter, next story
+      puts "single chapter story"
+      save(h,story,chapter)
+      break
+    end
   end
 end
 
-http = Net::HTTP.start "www.fanfiction.net"
-stories.each do |story|
-  u = "/s/#{story}/#{chapter}"
-  begin
-    h = http.get(u).body
-  rescue Exception
-    begin
-      http.close
-    rescue Exception
-    end
-    `say network error, resuming in 5 minutes`
-    sleep 300
-    http = Net::HTTP.start "www.fanfiction.net"
-    retry
-  end
-  if h.empty?
-    puts "story #{story} is blank, skipping"
-  elsif h =~ /Story Not Found/
-    # illegal id
-    puts "story #{story} is illegal, skipping"
-  elsif h =~ /chapter navigation/
-    # has chapters, save chapter, go to next
-    save(h,story,chapter) and next
-    chapter += 1
-  elsif chapter > 1
-    # last chapter
-    puts "last chapter found"
-    chapter = 1
-  elsif h =~ /Message Type 1/
-    # illegal id
-    puts "story #{story} is illegal, skipping"
-    chapter = 1
-  else
-    # single chapter, save chapter, next story
-    puts "single chapter story"
-    save(h,story,chapter)
-  end
-end
+$outputfile.close
+
+%x@ echo 'LOAD DATA INFILE "#{$outputfile.path}" INTO TABLE fanfiction_chapters FIELDS TERMINATED BY ";" (story,chapter,githash)' | mysql -u root corpora_development @
